2cd22fde5115b8396ef3aef18ecb0c09cc16d2f4
kevin <kevin@diamond-sky.com>
2020-10-10 14:12:39 +0100

    We're bowling (again)!

---
+++ Bowling/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bowling
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
